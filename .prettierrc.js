0c75a830c45c689e68b56ae132bffdb516ee6b11
Saad <saadsheikh27@gmail.com>
2022-06-01 23:34:39 +0500

    base

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "none",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: "always",
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: "preserve",
+    endOfLine: "crlf"
+};
+
